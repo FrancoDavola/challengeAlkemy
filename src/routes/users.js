6660988896fba7d68782aa42d16ddc9e101eb0fe
FrancoDavola <franko-davol@hotmail.com>
2022-03-20 18:01:40 -0300

    Registro y login

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,26 +1,25 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const {
-    getAllUsers, 
-    createUser, 
-    updateUser, 
-    getById, 
-    deleteUser
-} = require('../controllers/users');
-const { 
-    postRequestValidations,
-    putRequestValidations,
-    getAllRequestValidation,
-    getRequestValidation,
-    deleteRequestValidations
-} = require('../middlewares/users');
-
+  getAllUsers,
+  createUser,
+  updateUser,
+  getById,
+  deleteUser,
+} = require("../controllers/users");
+const {
+  postRequestValidations,
+  putRequestValidations,
+  getAllRequestValidation,
+  getRequestValidation,
+  deleteRequestValidations,
+} = require("../middlewares/users");
 
 const router = Router();
 
-router.get('/', getAllRequestValidation, getAllUsers);
-router.post('/', postRequestValidations, createUser);
-router.put('/:id', putRequestValidations, updateUser);
-router.get('/:id', getRequestValidation, getById);
-router.delete('/:id', deleteRequestValidations, deleteUser);
+router.get("/", getAllRequestValidation, getAllUsers);
+router.post("/", postRequestValidations, createUser);
+router.put("/:id(\\d+)/", putRequestValidations, updateUser);
+router.get("/:id(\\d+)/", getRequestValidation, getById);
+router.delete("/:id(\\d+)/", deleteRequestValidations, deleteUser);
 
 module.exports = router;
